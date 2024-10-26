15e8c90a6b781e86edc93f4e5940dcf982f04f35
Oleksandr Vasyliev <oleksandr.vasyliev04@gmail.com>
2024-10-26 16:12:33 +0300

    Add project files.

---
+++ Lab_1_Test_Proj/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Lab_1_Test_Proj")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab_1_Test_Proj")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("81722c52-370e-41c6-893e-e2a7ed10ab1f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Lab_1_Test_Proj/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data.SqlClient;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AnalaizerClassLibrary;
+using ErrorLibrary;
+
+namespace testing_lab1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private string connectionString = @"Data Source=DESKTOP-UMRBOQ7\MSSQLSERVER01;Initial Catalog=DBLibraryCalculator;Integrated Security=True;Connect Timeout=30";
+
+
+        [TestMethod]
+        public void FormatFromDB()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("SELECT InputExpression, ExpectedResult FROM FormatTestCases", connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    string inputExpression = reader.GetString(0);
+                    string expectedResult = reader.GetString(1);
+
+                    AnalaizerClass.expression = inputExpression;
+
+
+                    string result = AnalaizerClass.Format();
+
+                    if (expectedResult.StartsWith("&Error"))
+                    {
+                        StringAssert.StartsWith(result, expectedResult.Split(' ')[0]);
+                    }
+                    else
+                    {
+                        Assert.AreEqual(expectedResult, result);
+                    }
+                }
+                reader.Close();
+            }
+        }
+
+    }
+}
